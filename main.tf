4cbb796cbe1fc436be99ed9e1b21304dfffaca4c
mambotangu <mambo.tangu@sada.com>
2021-11-05 10:45:14 -0400

    commit1

---
+++ main.tf
@@ -0,0 +1,76 @@
+# The terraform {} block is required so Terraform knows which provider to download from the Terraform Registry.
+# In the configuration below, the google provider's source is defined as hashicorp/google which is shorthand 
+# for registry.terraform.io/hashicorp/google.
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+provider "google" {
+  project = "mamz-project-329009"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+  provisioner "local-exec" {
+    command = "echo ${google_compute_instance.vm_instance.name}: ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} >> ip_address.txt"
+  }
+# The local-exec provisioner executes a command locally on the machine running Terraform, not the VM instance itself.   
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+# The presence of the access_config block, even without any arguments, 
+# ensures that the instance will be accessible over the internet.
+        nat_ip = google_compute_address.vm_static_ip.address
+
+    }
+  }
+}
+# allocates a reserved IP address to your project
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
+
+# New resource for the storage bucket our application will use.
+resource "google_storage_bucket" "example_bucket" {
+  name     = "mamz-bucket6"
+  location = "US"
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+# Create a new instance that uses the bucket
+resource "google_compute_instance" "another_instance" {
+  # Tells Terraform that this VM instance must be created only after the
+  # storage bucket has been created.
+  depends_on = [google_storage_bucket.example_bucket]
+  name         = "terraform-instance-2"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
